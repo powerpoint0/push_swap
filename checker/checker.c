1df4a6111e4f1ccc2844ce2b87a99e81abcae121
Claudine Joaquin <cjoaquin@mi-s3.msk.21-school.ru>
2020-07-13 20:17:37 +0300

    read comands in checker

--- checker/checker.c
+++ checker/checker.c
@@ -20,44 +20,102 @@
 
 #include "../push_swap.h"
 
-void ft_do_comand(char *instr,int *a, int *b)
+void ft_do_comands(char *instr,int *a, int *b)
 {
 	if( !ft_strcmp(instr, "sa") || !ft_strcmp(instr, "sb"))
-		!ft_strcmp(instr, "sa") ? ft_sa_sb(a, "a") : ft_sa_sb(a, "b");
+		!ft_strcmp(instr, "sa") ? ft_sa_sb(a, 'a') : ft_sa_sb(a, 'b');
 	else if( !ft_strcmp(instr, "ss"))
 		ft_ss(a, b);
 	else if( !ft_strcmp(instr, "pa") || !ft_strcmp(instr, "pb"))
-		!ft_strcmp(instr, "pa") ? ft_pa_pb(a, b, "a") : ft_pa_pb(a, b, "b");
+		!ft_strcmp(instr, "pa") ? ft_pa_pb(a, b, 'a') : ft_pa_pb(a, b, 'b');
 	else if( !ft_strcmp(instr, "ra") || !ft_strcmp(instr, "rb"))
-		!ft_strcmp(instr, "ra") ? ft_ra_rb(a, "a") : ft_ra_rb(a, "b");
+		!ft_strcmp(instr, "ra") ? ft_ra_rb(a, 'a') : ft_ra_rb(a, 'b');
 	else if( !ft_strcmp(instr, "rr"))
 		ft_rr(a, b);
 	else if( !ft_strcmp(instr, "rra") || !ft_strcmp(instr, "rrb"))
-		!ft_strcmp(instr, "rra") ? ft_rra_rrb(a, "a") : ft_rra_rrb(a, "b");
+		!ft_strcmp(instr, "rra") ? ft_rra_rrb(a, 'a') : ft_rra_rrb(a, 'b');
 	else if( !ft_strcmp(instr, "rrr"))
 		ft_rrr(a, b);
 	else
-		return(-1);///error
+		return;///error
 	ft_bzero(instr, 5);
+}
+
+int ft_read_comands(char **instr)
+{
+	char buf[20];
+	int b;
+	int kolvo_n_buf;
+	int i;
+	int kolvo_n_all;
+	char **pointer;
+	char *n;
+
+	i = 0;
+	kolvo_n_buf = 0;
+	kolvo_n_all = 0;
+	ft_bzero(buf, 20);
+	while ((b = read(0, buf,19)))   ///ili simvol
+	{
+		pointer = instr;
+		while (buf[i])
+			kolvo_n_buf = (buf[i++] == '\n') ? kolvo_n_buf + 1 : kolvo_n_buf;
+		kolvo_n_all += kolvo_n_buf;
+		if (!(instr = (char**)malloc(sizeof(char*) * (kolvo_n_all + 1))))
+			return(-1);
+		i = 0;
+		while(pointer[i])
+		{
+			//malloc pod instr
+			ft_strcpy(instr[i], pointer[i]);
+			i++;
+		}
+		while(*pointer)
+		{
+			free(*pointer);
+			*pointer++;
+		}
+		n = buf;
+		int k = 0;
+		while((n = ft_strchr(n,'\n')))
+		{
+			while(n[k] != '\n')
+				k++;
+			//mallok instr sizeof k + 1
+			ft_memcpy(instr[i],n , k);
+			n = &n[k+1];
+			i++;
+		}
+		ft_bzero(buf, 20);
+	}
+	return(0);
 }
 
 int main(int argc, char **argv)
 {
 	int *a;
 	int *b;
-	char instr[5];
+	char **instr;
 
 	a = NULL;
 	b = NULL;
+	instr = NULL;
 	ft_check_error_and_write_numbers(argc, argv, a, b);
+	ft_read_comands(instr);
 	ft_bzero(instr, 5);
-	while(read <5 && read != 0)   //mozhno pridumat simvol dlya vyhoda  //read!= symvol//zapis v instr
-		ft_do_comand(instr, a, b);
+	while(*instr)
+		ft_do_comands(*instr, a, b);
 	if (b[0]!= 0 || !ft_check_sort(a))     //peredelat ft bez exit
-		write(1, "KO\n", 3)
+		write(1, "KO\n", 3);
 	else
 		write(1, "OK\n", 3);
 	free(a);
 	free(b);
+	while(*instr)
+	{
+		free(*instr);
+		*instr++;
+	}
+	free(instr);//polnost'yu postrochno
 	return(0);
 }
